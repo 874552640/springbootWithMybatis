b77a380afad9a13e71618d316b1f89719aae5ba1
ycc <874552640@qq.com>
2022-02-17 10:55:07 +0800

    start

---
+++ src/main/java/com/example/withmybatis/WithMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.withmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithMybatisApplication.class, args);
+    }
+
+}
